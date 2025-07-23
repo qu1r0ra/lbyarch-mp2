2898e9e8cb982153fdcc42f1509098fd24c34eb6
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-07-24 01:52:34 +0800

    docs: add asm starter documentation

--- src/asmfile.asm
+++ src/asmfile.asm
@@ -3,7 +3,11 @@ section .text
     default rel
     global imgCvtGrayFloatToInt
 
+; rcx   : int imgHeight         (height of the image in pixels)
+; rdx   : int imgWidth          (width of the image in pixels)
+; r8    : float *imgFloatRep    (pointer to an array of the image's float representation)
+; r9    : int *imgIntRep        (pointer to an array of the image's int representation)
 imgCvtGrayFloatToInt:
-    ;TODO: continue here
+    ; TODO: Implement the function
 
     ret
