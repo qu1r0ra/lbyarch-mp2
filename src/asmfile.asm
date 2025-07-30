8064466c52b7f02a3390ae4744f0439d37f8a61c
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-07-30 11:20:00 +0800

    feat: add imgCvtGrayFloatToInt functionality

--- src/asmfile.asm
+++ src/asmfile.asm
@@ -1,13 +1,24 @@
+section .data
+multiplier dd 255.0
+
 section .text
-    bits 64
-    default rel
-    global imgCvtGrayFloatToInt
+bits 64
+default rel
+global imgCvtGrayFloatToInt
 
 ; rcx   : int imgHeight         (height of the image in pixels)
 ; rdx   : int imgWidth          (width of the image in pixels)
 ; r8    : float *imgFloatRep    (pointer to an array of the image's float representation)
 ; r9    : int *imgIntRep        (pointer to an array of the image's int representation)
-imgCvtGrayFloatToInt:
-    ; TODO: Implement the function
+imgCvtGrayFloatToInt: ; TODO: Implement the function
+    imul rcx, rdx
 
+    L1: 
+        movss xmm1, [r8]
+        mulss xmm1, [multiplier]
+        cvtss2si eax, xmm1
+        mov [r9], eax
+        add r8, 4
+        add r9, 4
+        LOOP L1
     ret
